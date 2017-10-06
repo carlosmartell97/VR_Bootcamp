18358acdb9d9c39bfd88c13a6269db25c5a6da59
carlosmartell97 <carlosmartell97@gmail.com>
2017-10-06 18:41:25 -0500

    main project build. Allow camera movement around level.

---
+++ Unity_files/VR_Bootcamp/Assets/scripts/mainCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCamera : MonoBehaviour {
+
+	public int xVelocity, yVelocity;
+	public GameObject level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		this.transform.Translate(horizontal*xVelocity*Time.deltaTime,0,vertical*yVelocity*Time.deltaTime/*,Space.World*/);
+		this.transform.LookAt(level.transform.position);
+	}
+}
