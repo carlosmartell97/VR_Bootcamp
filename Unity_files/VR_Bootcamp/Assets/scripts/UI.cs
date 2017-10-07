9f36b45ac1306491ddc21e9657304cdb740267b6
carlosmartell97 <carlosmartell97@gmail.com>
2017-10-06 21:50:33 -0500

    initial UI configuration

---
+++ Unity_files/VR_Bootcamp/Assets/scripts/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	public Sprite arrowImage;
+	public Image arrowPic;
+
+	public void buttonForward(){
+		Debug.Log("move FORWARD!");
+		//GameObject.Find("sequence1").GetComponent<ImageChanger>().spriteToChangeTo = arrowImage;
+		GameObject.Find("sequence1").GetComponent<ImageChanger>().ChangeImage(arrowImage);
+		//GameObject.Find("sequence1").GetComponent<ImageChanger>().ChangeImage(arrowPic);
+	}
+
+	public void buttonRight(){
+		Debug.Log("move RIGHT!");
+	}
+
+	public void buttonLeft(){
+		Debug.Log("move LEFT!");
+	}
+
+}
