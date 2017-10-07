053e10ad96750d7149097ca53fc71b7c0ec76af5
carlosmartell97 <carlosmartell97@gmail.com>
2017-10-07 10:16:56 -0500

    shpere object moves with instructions told with UI

--- Unity_files/VR_Bootcamp/Assets/scripts/UI.cs
+++ Unity_files/VR_Bootcamp/Assets/scripts/UI.cs
@@ -5,22 +5,79 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
 
-	public Sprite arrowImage;
-	public Image arrowPic;
+	bool move, right, left, forward;
 
-	public void buttonForward(){
-		Debug.Log("move FORWARD!");
-		//GameObject.Find("sequence1").GetComponent<ImageChanger>().spriteToChangeTo = arrowImage;
-		GameObject.Find("sequence1").GetComponent<ImageChanger>().ChangeImage(arrowImage);
-		//GameObject.Find("sequence1").GetComponent<ImageChanger>().ChangeImage(arrowPic);
+	void Start(){
+		
 	}
 
-	public void buttonRight(){
-		Debug.Log("move RIGHT!");
+	void Update(){
+		if(move){
+			if(right){
+
+			}
+			else if(left){
+
+			}
+			else if(forward){
+
+			}
+		}
+	}
+
+	public Sprite arrowSprite;
+	//public Image arrowPic;
+
+	public void buttonForward(){
+		int steps = GameObject.Find("Camera").GetComponent<mainCamera>().steps;
+		GameObject.Find("Canvas").transform.GetChild(GameObject.Find("Camera").GetComponent<mainCamera>().steps+3).GetChild(0).gameObject.GetComponent<Image>().enabled = true;
+		GameObject.Find("Camera").GetComponent<mainCamera>().instructions[steps] = 1;
+		GameObject.Find("Camera").GetComponent<mainCamera>().steps++;
+		Debug.Log("move FORWARD!");
+		Debug.Log("steps:"+steps);
 	}
 
 	public void buttonLeft(){
+		int steps = GameObject.Find("Camera").GetComponent<mainCamera>().steps;
+		GameObject.Find("Canvas").transform.GetChild(GameObject.Find("Camera").GetComponent<mainCamera>().steps+3).GetChild(1).gameObject.GetComponent<Image>().enabled = true;
+		GameObject.Find("Camera").GetComponent<mainCamera>().instructions[steps] = 2;
+		GameObject.Find("Camera").GetComponent<mainCamera>().steps++;
 		Debug.Log("move LEFT!");
 	}
 
+	public void buttonRight(){
+		int steps = GameObject.Find("Camera").GetComponent<mainCamera>().steps;
+		GameObject.Find("Canvas").transform.GetChild(GameObject.Find("Camera").GetComponent<mainCamera>().steps+3).GetChild(2).gameObject.GetComponent<Image>().enabled = true;
+		GameObject.Find ("Camera").GetComponent<mainCamera>().instructions[steps] = 3;
+		GameObject.Find("Camera").GetComponent<mainCamera>().steps++;
+		Debug.Log("move RIGHT!");
+	}
+
+	public void startButton(){
+		int[] ins = GameObject.Find("Camera").GetComponent<mainCamera>().instructions;
+		Debug.Log("STARTA!");
+		move = true;
+		for(int i=0; i<6; i++){
+			Debug.Log("at index:"+ins[i]);
+			if(ins[i]==1){
+				GameObject.Find("Sphere").transform.Translate(0,0,3,Space.Self);
+				StartCoroutine(waitSeconds());
+			}
+			else if(ins[i]==2){
+				GameObject.Find("Sphere").transform.Rotate(0,-90,0);
+				StartCoroutine(waitSeconds());
+			}
+			else if(ins[i]==3){
+				right = true;
+				GameObject.Find("Sphere").transform.Rotate(0,90,0);
+				StartCoroutine(waitSeconds());
+			}
+		}
+	}
+
+	IEnumerator waitSeconds(){
+		while (true) {
+			yield return new WaitForSeconds(10.0f);
+		}
+	}
 }
--- Unity_files/VR_Bootcamp/Assets/scripts/mainCamera.cs
+++ Unity_files/VR_Bootcamp/Assets/scripts/mainCamera.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mainCamera : MonoBehaviour {
 
 	public int xVelocity, yVelocity;
 	public GameObject level;
+	public int steps;
+	public int[] instructions;
 
 	// Use this for initialization
 	void Start () {
-		
+		instructions = new int[6];
+		for(int i=3; i<9; i++){
+			for(int j=0; j<3; j++){
+				GameObject.Find ("Canvas").transform.GetChild (i).GetChild (j).gameObject.GetComponent<Image> ().enabled = false;
+			}
+		}
+		this.steps = 0;
 	}
 	
 	// Update is called once per frame
